a525984817e41c1530123c3f29666f7c6ca4988e
MayankNailwaldev <mayanknailwal6@gmail.com>
2026-05-26 13:18:59 +0530

    fix server startup for render

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
-require("dotenv").config();
-
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
 const connectDB = require("./config/db");
+
 const userRoutes = require("./routes/userRoutes");
 const medicineRoutes = require("./routes/medicineRoutes");
 const orderRoutes = require("./routes/orderRoutes");
@@ -12,16 +13,27 @@ connectDB();
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use("/api/users", userRoutes);
-app.use("/api/medicines", medicineRoutes);
-app.use("/api/orders", orderRoutes);
+app.use(
+  express.json({
+    limit: "50mb",
+  }),
+);
+
+app.use(
+  express.urlencoded({
+    limit: "50mb",
+    extended: true,
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("HealGo API Running 🚀");
 });
+
+app.use("/api/users", userRoutes);
+app.use("/api/medicines", medicineRoutes);
+app.use("/api/orders", orderRoutes);
 
 const PORT = process.env.PORT || 5000;
 
